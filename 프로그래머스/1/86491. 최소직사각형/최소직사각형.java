af9016987add7511e7e9b0654be958e0bd4b3a36
Jonghun Lee <jonghuncu@gmail.com>
2025-10-26 09:46:44 +0900

    [level 1] Title: 최소직사각형, Time: 1.76 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -3,69 +3,89 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int n = sizes.length;
-        List<Wallet> wList = new ArrayList<Wallet>();
+        int maxVertical = 0;
+        int maxHeight = 0;
         
-        for(int[] ls : sizes) {
-            int x = ls[0];
-            int y = ls[1];
+        for (int i=0; i<sizes.length; i++) {
+            int max = Math.max(sizes[i][0], sizes[i][1]);
+            int min = Math.min(sizes[i][0], sizes[i][1]);
             
-            // x가 항상 더 길게
-            if (y > x) {
-                int tmp = x;
-                x = y;
-                y = tmp;
-            }
-            
-            Wallet wallet = new Wallet(x, y);
-            wList.add(wallet);
+            maxVertical = Math.max(maxVertical, max);
+            maxHeight = Math.max(maxHeight, min);
         }
         
-        List<Integer> xList = new ArrayList<>();
-        List<Integer> yList = new ArrayList<>();
-        
-        for (Wallet w : wList) {
-            int x = w.getW();
-            xList.add(x);
-            print(x);
-        }
-        
-        for (Wallet w : wList) {
-            int y = w.getH();
-            yList.add(y);
-            print(y);
-        }
-        
-        Integer xMax = Collections.max(xList);
-        Integer yMax = Collections.max(yList);
-        
-            
-        int answer = 0;
-        
-        answer = xMax * yMax;
+        int answer = maxVertical * maxHeight;
         return answer;
     }
     
-    public static class Wallet {
-        private int w;
-        private int h;
-        
-        public Wallet(int w, int h) {
-            this.w = w;
-            this.h = h;
-        }
-        
-        public int getW() {
-            return this.w;
-        }
-        
-        public int getH() {
-            return this.h;
-        }
-        
-    }
-    
-    public static void print(int v) {
-        System.out.println(v);
-    }
 }
+
+
+// class Solution {
+//     public int solution(int[][] sizes) {
+//         int n = sizes.length;
+//         List<Wallet> wList = new ArrayList<Wallet>();
+        
+//         for(int[] ls : sizes) {
+//             int x = ls[0];
+//             int y = ls[1];
+            
+//             // x가 항상 더 길게
+//             if (y > x) {
+//                 int tmp = x;
+//                 x = y;
+//                 y = tmp;
+//             }
+            
+//             Wallet wallet = new Wallet(x, y);
+//             wList.add(wallet);
+//         }
+        
+//         List<Integer> xList = new ArrayList<>();
+//         List<Integer> yList = new ArrayList<>();
+        
+//         for (Wallet w : wList) {
+//             int x = w.getW();
+//             xList.add(x);
+//             print(x);
+//         }
+        
+//         for (Wallet w : wList) {
+//             int y = w.getH();
+//             yList.add(y);
+//             print(y);
+//         }
+        
+//         Integer xMax = Collections.max(xList);
+//         Integer yMax = Collections.max(yList);
+        
+            
+//         int answer = 0;
+        
+//         answer = xMax * yMax;
+//         return answer;
+//     }
+    
+//     public class Wallet {
+//         private int w;
+//         private int h;
+        
+//         public Wallet(int w, int h) {
+//             this.w = w;
+//             this.h = h;
+//         }
+        
+//         public int getW() {
+//             return this.w;
+//         }
+        
+//         public int getH() {
+//             return this.h;
+//         }
+        
+//     }
+    
+//     public static void print(int v) {
+//         System.out.println(v);
+//     }
+// }
